78d6dc4ea1868a72c6cabff1ce2bd87452d2442d
Kalelzar <natomanofglory@gmail.com>
2021-07-25 14:49:22 +0300

    Add missing 'else'

--- src/AST/Funcall.cpp
+++ src/AST/Funcall.cpp
@@ -19,25 +19,25 @@ ElementPtr Funcall::optimal() {
   if (name->identifier == "add" && args.size() == 2) {
     auto add = std::make_shared<Add>(args.front(), args.back())->optimal();
     return add;
-  } if (name->identifier == "sub" && args.size() == 2) {
+  } else if (name->identifier == "sub" && args.size() == 2) {
     auto sub = std::make_shared<Sub>(args.front(), args.back())->optimal();
     return sub;
-  } if (name->identifier == "mul" && args.size() == 2) {
+  } else if (name->identifier == "mul" && args.size() == 2) {
     auto mul = std::make_shared<Mul>(args.front(), args.back())->optimal();
     return mul;
-  } if (name->identifier == "div" && args.size() == 2) {
+  } else if (name->identifier == "div" && args.size() == 2) {
     auto div = std::make_shared<Div>(args.front(), args.back())->optimal();
     return div;
-  } if (name->identifier == "pow" && args.size() == 2) {
+  } else if (name->identifier == "pow" && args.size() == 2) {
     auto pow = std::make_shared<Pow>(args.front(), args.back())->optimal();
     return pow;
-  } if (name->identifier == "eq" && args.size() == 2) {
+  } else if (name->identifier == "eq" && args.size() == 2) {
     auto eq = std::make_shared<Eq>(args.front(), args.back())->optimal();
     return eq;
-  } if (name->identifier == "le" && args.size() == 2) {
+  } else if (name->identifier == "le" && args.size() == 2) {
     auto le = std::make_shared<Le>(args.front(), args.back())->optimal();
     return le;
-  } if (name->identifier == "nand" && args.size() == 2) {
+  } else if (name->identifier == "nand" && args.size() == 2) {
     auto nand = std::make_shared<Nand>(args.front(), args.back())->optimal();
     return nand;
   } else if (name->identifier == "if" && args.size() == 3) {
