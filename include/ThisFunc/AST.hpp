93c06ff8016a68c4aa8c45bdd6dc80d320c10cb9
Kalelzar <natomanofglory@gmail.com>
2021-07-25 14:48:00 +0300

    Remove redundant functions

--- include/ThisFunc/AST.hpp
+++ include/ThisFunc/AST.hpp
@@ -9,43 +9,53 @@
 namespace ThisFunc {
 namespace AST {
 
-
-
 template <class A> using ASTPointer = std::shared_ptr<A>;
-
-
 
 class Element {
 public:
+  /**
+   * @brief      Print the AST node to stdout
+   *
+   * @details    Uses a Lisp-like syntax.
+   */
   virtual void print() = 0;
 
+  /**
+   * @brief      Return an optimal version of the AST node
+   */
   virtual ASTPointer<Element> optimal() = 0;
+  /// @brief Returns true if the instance is and instance of Number and false
+  /// otherwise
   virtual bool isNumber() { return false; }
+  /// @brief Returns true if the instance is and instance of Identifier and
+  /// false otherwise
   virtual bool isIdentifier() { return false; }
-  virtual Element *printf() {
-    print();
-    return this;
-  }
 };
 
+/**
+ * @brief      Casts an ASTPointer<B> into ASTPointer<A>
+ *
+ * @details    A wrapper around std::static_pointer_cast that ensures the
+ *             arguments are ASTPointer for better static type-checking
+ *
+ *
+ * @param      b the old pointer
+ * @type_param A the type of the new pointer
+ * @type_param B the type of the old pointer
+ * @return     the new pointer
+ */
 template <typename A, typename B>
-AST::ASTPointer<A> ptr_cast(AST::ASTPointer<B> b){
+AST::ASTPointer<A> ptr_cast(AST::ASTPointer<B> b) {
   return std::static_pointer_cast<A>(b);
 }
 
 using ElementPtr = ASTPointer<Element>;
 
-class Statement : public Element {
-public:
-  virtual void print() override = 0;
-};
+class Statement : public Element {};
 
 using StatementPtr = ASTPointer<Statement>;
 
-class Expression : public Statement {
-public:
-  virtual void print() override = 0;
-};
+class Expression : public Statement {};
 using ExpressionPtr = ASTPointer<Expression>;
 
 class Number : public Expression {
@@ -103,8 +113,8 @@ class Body : public Element {
 public:
   void print() override;
   ElementPtr optimal() override;
-  Body(std::list<StatementPtr>& statements) : statements(statements) {}
-  Body(std::list<StatementPtr>&& statements) : statements(statements) {}
+  Body(std::list<StatementPtr> &statements) : statements(statements) {}
+  Body(std::list<StatementPtr> &&statements) : statements(statements) {}
   std::list<StatementPtr> statements;
 };
 
