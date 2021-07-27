0b13acb4e54a937e152d7aa9c361ddb4c3ef19eb
Kalelzar <natomanofglory@gmail.com>
2021-07-27 21:39:23 +0300

    Update main loop

--- exec/main.cpp
+++ exec/main.cpp
@@ -1,16 +1,39 @@
+#include "ThisFunc/Chunk.hpp"
 #include <iostream>
 
 #include <ThisFunc/Parser.hpp>
 #include <ThisFunc/Interpreter.hpp>
 
+#include <ThisFunc/VM.hpp>
+#include <ThisFunc/Disassembler.hpp>
+#include <ThisFunc/Compiler.hpp>
+
 int main(){
 
-  ThisFunc::Parser s(&std::cin);
-  ThisFunc::AST::BodyPtr ptr =  s.parse();
-  ThisFunc::Interpreter interpreter(ptr);
-  ptr->print();
-  interpreter.precalculate();
-  //  interpreter.print();
+  ThisFunc::VM::Chunk chunk;
 
-  return 0;
+  ThisFunc::Compiler compiler;
+
+
+  if(compiler.compile(&std::cin, &chunk)){
+
+    ThisFunc::VM::disassemble(chunk);
+
+    ThisFunc::VM::VM vm;
+    if(vm.interpret(&chunk)){
+      return 0;
+    }else {
+      return 1;
+    }
+  }
+
+  // ThisFunc::Parser s(&std::cin);
+  // ThisFunc::AST::BodyPtr ptr =  s.parse();
+  // if(s.hadError) return 1;
+  // ThisFunc::Interpreter interpreter(ptr);
+  // ptr->print();
+  // interpreter.precalculate();
+  // //  interpreter.print();
+
+  return 2;
 }
