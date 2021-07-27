1b40324b8a8337393bb2f8ada00f3cabb5878628
Kalelzar <natomanofglory@gmail.com>
2021-07-27 21:36:34 +0300

    Implement dynamic array

---
+++ include/ThisFunc/DynArray.hpp
@@ -0,0 +1,126 @@
+#ifndef DYNARRAY_H
+#define DYNARRAY_H
+
+#include <exception>
+#include <stdexcept>
+
+#include <ThisFunc/defs.hpp>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+#ifndef THISFUNC_DEFAULT_CHUNKSIZE
+#define THISFUNC_DEFAULT_CHUNKSIZE 8
+#endif
+
+namespace ThisFunc {
+
+template <typename Data> class DynArray {
+public:
+  //! Default constructor
+  DynArray() : capacity(0), size(0), storage((Data *)0xdeadbeef) {}
+
+  //! Copy constructor
+  DynArray(const DynArray<Data> &other) { copy(other); }
+
+  //! Move constructor
+  DynArray(DynArray<Data> &&other) noexcept { move(std::move(other)); }
+
+  //! Destructor
+  virtual ~DynArray() noexcept {
+    if (storage && storage != (Data *)0xdeadbeef)
+      free(storage);
+  }
+
+  //! Copy assignment operator
+  DynArray &operator=(const DynArray &other) { return copy(other); }
+
+  //! Move assignment operator
+  DynArray &operator=(DynArray &&other) noexcept {
+    return move(std::move(other));
+  }
+
+  u32 getSize() const { return size; }
+
+  u32 getCapacity() const { return size; }
+
+  u64 address() const { return (u64)storage; }
+
+  Data operator[](u32 index) {
+    if (capacity == 0) {
+      throw std::runtime_error("Accessing uninitialized memory.");
+    }
+    if (index >= size)
+      return storage[size - 1];
+    return storage[index];
+  }
+
+  const Data operator[](u32 index) const {
+    if (capacity == 0) {
+      throw std::runtime_error("Accessing uninitialized memory.");
+    }
+    if (index >= size)
+      return storage[size - 1];
+    return storage[index];
+  }
+
+  u32 write(Data next) {
+    if (capacity < size + 1)
+      expand();
+    storage[size] = next;
+    return size++;
+  }
+
+  Data* getStorage() {
+    return storage;
+  }
+
+protected:
+  void expand() {
+    u32 newCapacity = capacity ? capacity * 2 : THISFUNC_DEFAULT_CHUNKSIZE;
+    if (storage == (Data *)0xdeadbeef)
+      storage = static_cast<Data *>(malloc(sizeof(Data) * newCapacity));
+    else
+      storage =
+          static_cast<Data *>(realloc(storage, sizeof(Data) * newCapacity));
+
+    if (storage == nullptr) {
+      // Failed to allocate memory for the storage buffer
+      // There is nothing we can do.
+      std::cerr << "FATAL ERROR: Failed to allocate storage chunk of size "
+                << newCapacity << ". Aborting.";
+      exit(63);
+    }
+    capacity = newCapacity;
+  }
+
+  DynArray<Data> &copy(const DynArray<Data> &other) {
+    if (&other != this) {
+      storage =
+          static_cast<Data *>(memcpy(storage, other.storage, other.capacity));
+      capacity = other.capacity;
+      size = other.size;
+    }
+    return *this;
+  }
+
+  DynArray<Data> &move(DynArray<Data> &&other) {
+    if (&other != this) {
+      storage = other.storage;
+      other.storage = nullptr;
+      capacity = other.capacity;
+      size = other.size;
+      other.capacity = 0;
+      other.size = 0;
+    }
+    return *this;
+  }
+
+private:
+
+  Data *storage;
+  u32 capacity, size;
+};
+} // namespace ThisFunc
+
+#endif /* DYNARRAY_H */
